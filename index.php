d79ee63dba97a8347a72daf9fe27d2eff8489b18
ImMarw <jan.mares@student.ossp.cz>
2024-10-15 12:19:03 +0200

    toast button

--- index.php
+++ index.php
@@ -36,5 +36,31 @@
         ?>
     </div>
 </div>
+<button type="button" class="btn btn-primary" onclick="toaster()" id="liveToastBtn">Show live toast</button>
+
+<div class="toast-container position-fixed bottom-0 end-0 p-3">
+  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <img src="..." class="rounded me-2" alt="...">
+      <strong class="me-auto">Bootstrap</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      Hello, world! This is a toast message.
+    </div>
+  </div>
+</div>
+<script>
+    function toaster(){
+    const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}}
+</script>
 <?php include 'templates/footer.php'; ?>
 </div>
